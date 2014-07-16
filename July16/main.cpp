59945bcd5944de3d917e32f08312898b4e2bfb04
Grace Kendall <gkendall@apple.com>
2014-07-16 10:10:13 -0700

    Initial Commit

---
+++ July16/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  July16
+//
+//  Created by Grace Kendall on 7/16/14.
+//  Copyright (c) 2014 com.example. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
